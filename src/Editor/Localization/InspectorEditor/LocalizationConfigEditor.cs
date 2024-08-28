ca06d8fb91003ce585aeab04c76100baabfe59d2
car-rot1 <1315883482@qq.com>
2024-08-28 18:56:39 +0800

    优化了LocalizationConfig的绘制；

--- src/Editor/Localization/InspectorEditor/LocalizationConfigEditor.cs
+++ src/Editor/Localization/InspectorEditor/LocalizationConfigEditor.cs
@@ -10,9 +10,9 @@ namespace CGame.Localization.Editor
     public class LocalizationConfigEditor : UnityEditor.Editor
     {
         private int _defaultLanguageIndex;
-        private List<string> _allLanguages;
-        
-        private LocalizationConfig _target;
+        private List<string> _allLanguage;
+
+        private IEnumerable<Type> _allLocalizationAssetExternalLoaderType;
 
         private SerializedProperty _languages;
         private SerializedProperty _defaultLanguage;
@@ -24,7 +24,6 @@ namespace CGame.Localization.Editor
         
         private void OnEnable()
         {
-            _target = (LocalizationConfig)target;
             _languages = serializedObject.FindProperty("languages");
             _defaultLanguage = serializedObject.FindProperty("defaultLanguage");
             _stringInternalLoader = serializedObject.FindProperty("stringInternalLoader");
@@ -50,7 +49,8 @@ namespace CGame.Localization.Editor
                 _defaultLanguage.stringValue = languagesSize > 0 ? _languages.GetArrayElementAtIndex(0).stringValue : "";
             }
 
-            _allLanguages = new List<string>();
+            _allLanguage = new List<string>();
+            _allLocalizationAssetExternalLoaderType = TypeCache.GetTypesDerivedFrom<LocalizationAssetExternalLoaderBase>().Where(type => !type.IsAbstract);
         }
         
         public override void OnInspectorGUI()
@@ -63,31 +63,35 @@ namespace CGame.Localization.Editor
             
             EditorGUILayout.PropertyField(_languages);
             
-            _allLanguages.Clear();
+            _allLanguage.Clear();
             for (var i = 0; i < _languages.arraySize; i++)
             {
-                _allLanguages.Add(_languages.GetArrayElementAtIndex(i).stringValue);
+                _allLanguage.Add(_languages.GetArrayElementAtIndex(i).stringValue);
             }
             
             EditorGUI.BeginChangeCheck();
-            _defaultLanguageIndex = EditorGUILayout.Popup(_defaultLanguage.displayName, _defaultLanguageIndex, _allLanguages.ToArray());
+            _defaultLanguageIndex = EditorGUILayout.Popup(_defaultLanguage.displayName, _defaultLanguageIndex, _allLanguage.ToArray());
             if (EditorGUI.EndChangeCheck())
-                _defaultLanguage.stringValue = _allLanguages[_defaultLanguageIndex];
+                _defaultLanguage.stringValue = _allLanguage[_defaultLanguageIndex];
             
             EditorGUILayout.PropertyField(_stringInternalLoader);
             EditorGUILayout.PropertyField(_assetInternalLoader);
             
             EditorGUILayout.PropertyField(_stringExternalLoader);
             EditorGUILayout.PropertyField(_assetExternalLoaders);
+            
+            if (_allLocalizationAssetExternalLoaderType.Count() == _assetExternalLoaders.arraySize)
+                return;
+            
             if (GUILayout.Button("AddLoader"))
             {
                 var menu = new GenericMenu();
-                foreach (var type in TypeCache.GetTypesDerivedFrom<LocalizationAssetExternalLoaderBase>().Where(type => !type.IsAbstract))
+                foreach (var type in _allLocalizationAssetExternalLoaderType)
                 {
                     int i;
                     for (i = 0; i < _assetExternalLoaders.arraySize; i++)
                     {
-                        if (_assetExternalLoaders.GetArrayElementAtIndex(i).boxedValue.GetType() == type)
+                        if (_assetExternalLoaders.GetArrayElementAtIndex(i).managedReferenceValue.GetType() == type)
                             break;
                     }
                     if (i < _assetExternalLoaders.arraySize)
@@ -97,7 +101,7 @@ namespace CGame.Localization.Editor
                         var loader = Activator.CreateInstance(type);
                         var length = _assetExternalLoaders.arraySize;
                         _assetExternalLoaders.InsertArrayElementAtIndex(length);
-                        _assetExternalLoaders.GetArrayElementAtIndex(length).boxedValue = loader;
+                        _assetExternalLoaders.GetArrayElementAtIndex(length).managedReferenceValue = loader;
                         _assetExternalLoaders.serializedObject.ApplyModifiedProperties();
                     });
                 }
